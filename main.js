3896cb78a073ad15e83ebd9c481143b2acedbb14
amarisodiq <amarisodiq@gmail.com>
2025-04-17 15:51:16 +0100

    edit htaccess

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    const navbarCollapse = document.getElementById('navbarSupportedContent');
+
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+          toggle: false
+        });
+        bsCollapse.hide();
+      });
+    });
+  });
